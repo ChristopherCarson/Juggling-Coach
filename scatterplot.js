bbc7738945f762bf2d891fc3c7a3450063958b2b
John Coffelt <33336503+JohnRCoffelt@users.noreply.github.com>
2019-05-05 15:24:35 -0700

    Update scatterplot.js

--- scatterplot.js
+++ scatterplot.js
@@ -1,9 +1,9 @@
-			var data = {
-			  x: [1, 2, 3, 4],
-			  y: [10, 15, 13, 17],
-			  mode: 'markers'
-			};
-      
-			var layout = {};
+var data = {
+  x: [1, 2, 3, 4],
+  y: [10, 15, 13, 17],
+  mode: 'markers'
+};
 
-			Plotly.newPlot('scatterPlot', data, layout);
+var layout = {};
+
+Plotly.newPlot('scatterPlot', data, layout);
