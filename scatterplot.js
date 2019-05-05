359f54894a2d17933f65fb77f58e117f99d9a978
John Coffelt <33336503+JohnRCoffelt@users.noreply.github.com>
2019-05-05 16:41:57 -0700

    Update scatterplot.js

--- scatterplot.js
+++ scatterplot.js
@@ -1,3 +1,4 @@
+  
 function rand() {
   return Math.random();
 }  
@@ -8,31 +9,35 @@ function addToPlot(markerType, xCoord, yCoord) {
 	}, [markerType])
 }
 
-var layout = {
-	autosize: false,
-	width:420,
-	height:440,
-	margin: {
-		l: 25,
-		r: 25,
-		b: 50,
-		t: 50,
-		pad: 4
-    }
-};
+function createPlot() {
+	var layout = {
+		autosize: false,
+		width:500,
+		height:440,
+		margin: {
+			l: 25,
+			r: 25,
+			b: 50,
+			t: 50,
+			pad: 4
+		}
+	};
 
-Plotly.plot('scatterPlot', [
-{
-  y: [].map(rand),
-  x: [].map(rand),
-  mode: 'markers',
-  line: {color: '#0FE60B'},
-  name: "Color"
-},
-{
-  y: [].map(rand),
-  x: [].map(rand),
-  mode: 'markers',
-  line: {color: '#E60B0B'},
-  name: "Motion"
-}], layout);
+	Plotly.plot('scatterPlot', [
+	{
+	  y: [].map(rand),
+	  x: [].map(rand),
+	  mode: 'markers',
+	  line: {color: '#0FE60B'},
+	  name: "Color"
+	},
+	{
+	  y: [].map(rand),
+	  x: [].map(rand),
+	  mode: 'markers',
+	  line: {color: '#E60B0B'},
+	  name: "Motion"
+	}], layout);
+}
+
+createPlot();
