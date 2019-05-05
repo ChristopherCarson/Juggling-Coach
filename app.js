b796febeed709e476635264eecfd5f42a85168ce
John Coffelt <33336503+JohnRCoffelt@users.noreply.github.com>
2019-05-05 16:26:28 -0700

    Update app.js

--- app.js
+++ app.js
@@ -208,7 +208,7 @@ function onVideoStarted() {
                         });
                     }
                     //Add to scatterPlot
-                    addToPlot(0, centerColor.x, (centerColor.y - (centerColor.y * 2))
+                    addToPlot(0, centerColor.x, (centerColor.y - (centerColor.y * 2)));
                 }
             }
 
@@ -232,7 +232,7 @@ function onVideoStarted() {
                         });
                     }
                     //Add to scatterPlot
-                    addToPlot(1, centerMotion.x, (centerMotion.y - (centerMotion.y * 2))
+                    addToPlot(1, centerMotion.x, (centerMotion.y - (centerMotion.y * 2)));
                 }
             }
 
